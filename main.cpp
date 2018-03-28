d3bad6f01710ffca12634b7a75063356260afb3d
RustyRaptor <ziadarafat1@gmail.com>
2018-03-28 13:26:26 -0600

    initial commit, fixed main not returning integer

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Student on 3/28/2018.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "What's your favorite number?";
+    cin >> n;
+
+    n+=1;
+
+    cout << endl;
+    cout << "My favorite number is 1 mo4re than that"
+         << n << endl;
+    cout << endl;
+    cout << endl << endl;
+}
