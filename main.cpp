7d32f5ca9dca37b8ea4b374d0915645e00215982
RustyRaptor <anonymous0programmer@gmail.com>
2018-03-28 23:20:38 -0600

    added some comments and reformatted

--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
+
+
 #include <iostream>
 
-
-// use the names int he standard library directly rather than having to call the stdlib manually each time.
+// use the names int he standard library directly rather than having to call the
+// stdlib manually each time.
 using namespace std;
 
-// main function needed to return an int for some reason instead of void. Probably because the program does return ints
-int main(){
-    int n;
+int main() {
+	int n;
 
-    cout << "What's your favorite number?, ";
-    cin >> n;
+	cout << "What's your favorite number?, ";
+	// set n to the user's input
+	cin >> n;
+
+	// add one to n
+	n += 1;
 
 
-    n+=1;
+	cout << endl; // writes a newline
+	cout << "My favorite number is 1 more than that, " << n << endl;
+	cout << endl;
+	cout << endl << endl; // writes 3 new lines (kind of redundant)
 
-    cout << endl; // writes a newline
-    cout << "My favorite number is 1 more than that, "
-         << n << endl; // prints n and a newline
-    cout << endl; // writes 3 new lines (kind of redundant)
-    cout << endl << endl;
+	return 0;
 }
